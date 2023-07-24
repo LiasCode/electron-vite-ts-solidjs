f3efcf464978dec020d7c2a9b766772b410de3b2
LiasCode <liasraul00@gmail.com>
2023-07-23 23:29:13 -0400

    add electron and typescript logos to example page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { createSignal } from "solid-js";
 import solidLogo from "@/assets/solid.svg";
 import viteLogo from "@/assets/vite.svg";
+import electronLogo from "@/assets/Electron_Software_Framework_Logo.svg.png";
+import typescriptLogo from "@/assets/typescript-design-assets/ts-logo-512.png";
 import "@/App.css";
 
 function App() {
@@ -10,12 +12,32 @@ function App() {
     <>
       <div>
         <a
+          href="https://www.electronjs.org/"
+          target="_blank"
+        >
+          <img
+            src={electronLogo}
+            class="logo electron"
+            alt="Electron Logo"
+          />
+        </a>
+        <a
+          href="https://www.typescriptlang.org/"
+          target="_blank"
+        >
+          <img
+            src={typescriptLogo}
+            class="logo ts"
+            alt="TypeScript Logo"
+          />
+        </a>
+        <a
           href="https://vitejs.dev"
           target="_blank"
         >
           <img
             src={viteLogo}
-            class="logo"
+            class="logo vite"
             alt="Vite logo"
           />
         </a>
@@ -30,7 +52,9 @@ function App() {
           />
         </a>
       </div>
-      <h1>Vite + Solid</h1>
+
+      <h1>Electron + TypeScript + Vite + Solid</h1>
+
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count()}
@@ -39,9 +63,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+
       <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
+        Click on the Electron, TypeScript, Vite and Solid logos to learn more
       </p>
+
     </>
   );
 }
